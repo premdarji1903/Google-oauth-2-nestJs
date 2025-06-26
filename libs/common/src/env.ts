f2c77ea56e11afbccf43acd31670afeceb874cad
prem darji <darjipren08@gmail.com>
2025-06-26 22:53:44 +0530

    Add new env cond

--- libs/common/src/env.ts
+++ libs/common/src/env.ts
@@ -3,4 +3,4 @@ config()
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 export const GOOGLE_ACCESS_TOKEN_URL = process.env.GOOGLE_ACCESS_TOKEN_URL;
-export const REDIRECT_URL=process.env.REDIRECT_URL
+export const REDIRECT_URL=process.env.NODE_ENV =="DEV" ? process.env.DEV_REDIRECT_URL : process.env.PROD_REDIRECT_URL
